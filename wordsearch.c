53bbeb286dd4805c267dd65d0ff12d174c30508a
Gandledore <rsmilstrey@gmail.com>
2023-02-13 12:22:00 -0800

    recursive findnext
    search for first letter, then next letters
    some helper functions
    free memory

--- wordsearch.c
+++ wordsearch.c
@@ -4,9 +4,102 @@
 
 // Declarations of the two functions you will implement
 // Feel free to declare any helper functions or global variables
-void printPuzzle(char** arr);
-void searchPuzzle(char** arr, char* word);
+typedef struct point{
+    int row;
+    int col;
+} point;
 int bSize;
+int length=0;
+point * surroundings;
+int** output;
+void printPuzzle(char** arr) {
+	// This function will print out the complete puzzle grid (arr). 
+    // It must produce the output in the SAME format as the samples 
+    // in the instructions.
+    // Your implementation here...
+
+}
+void printpoint(point p){
+    printf("%d, %d",p.row,p.col);
+}
+point addPoints(point a, point b){
+    return (point){a.row+b.row,a.col+b.col};
+}
+char checkPoint(char** arr, point p){
+    return *(*(arr+p.row)+p.col);
+}
+void writetoGlobal(point* list){
+    for(int k=0;k<length;k++){
+        *(*(output+(list+k)->row)+(list+k)->col) = *(*(output+(list+k)->row)+(list+k)->col)*10+k+1;
+    }
+}
+void fn(char** block, char* word, point* list, int index){
+    for(int i=0;i<8;i++){
+        point p = addPoints(*(list+index-1),*(surroundings+i));
+        if(p.row==-1 || p.col==-1 || p.row==bSize || p.col==bSize){
+            continue;
+        }
+        if(checkPoint(block,p)==*(word+index)){
+            *(list+index)=p;
+            if(index!=length){
+                fn(block,word,list,index+1);
+            }
+            else{
+                writetoGlobal(list);
+            }
+        }
+    }
+    *(list+index)=(point){-1,-1};
+}
+void searchPuzzle(char** block, char* word) {
+    while(*(word+length)!='\0'){
+        length++;
+    }
+    output = malloc(bSize*sizeof(int*));
+    for(int i=0;i<bSize;i++){
+        *(output+i)=malloc(bSize*sizeof(int));
+        for(int j=0;j<bSize;j++){
+            *(*(output+i)+j)=0;
+        }
+    }
+    if(surroundings = malloc(8*sizeof(point)));{//if not void then define surrouding shift
+        *(surroundings) = (point){-1,-1};
+        *(surroundings+1) = (point){-1,0};
+        *(surroundings+2) = (point){-1,1};
+        *(surroundings+3) = (point){0,-1};
+        *(surroundings+4) = (point){0,1};
+        *(surroundings+5) = (point){1,-1};
+        *(surroundings+6) = (point){1,0};
+        *(surroundings+7) = (point){1,1};
+    }
+    point* path = malloc(length*sizeof(point));
+    for(int i=0;i<bSize;i++){
+        for(int j=0;j<bSize;j++){
+            if(*(*(block+i)+j)==*word){
+                *path = (point){i,j};
+                fn(block, word, path,1);
+            }
+        }
+    }
+
+    // This function checks if arr contains the search word. If the 
+    // word appears in arr, it will print out a message and the path 
+    // as shown in the sample runs. If not found, it will print a 
+    // different message as shown in the sample runs.
+    // Your implementation here...
+
+    //free memory
+    free(word);
+    for(int i=0;i<bSize;i++){
+        free(*(block+i));
+        free(*(output+i));
+    }
+    free(block);
+    free(output);
+    free(surroundings);
+    free(path);
+
+}
 
 // Main function, DO NOT MODIFY 	
 int main(int argc, char **argv) {
@@ -50,39 +143,6 @@ int main(int argc, char **argv) {
     
     // Call searchPuzzle to the word in the puzzle
     searchPuzzle(block, word);
-    
+
     return 0;
-}
-
-void printPuzzle(char** arr) {
-	// This function will print out the complete puzzle grid (arr). 
-    // It must produce the output in the SAME format as the samples 
-    // in the instructions.
-    // Your implementation here...
-
-    int i;
-    for(i = 0; i < bSize; i++){
-        int j;
-        for(j = 0; j < bSize; j++){
-            printf("%c ",*(*(arr + i) + j));
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-}
-
-void printAnswer(int** ans) {
-    //This function will print the answer to the array
-
-
-}
-
-void searchPuzzle(char** arr, char* word) {
-    // This function checks if arr contains the search word. If the 
-    // word appears in arr, it will print out a message and the path 
-    // as shown in the sample runs. If not found, it will print a 
-    // different message as shown in the sample runs.
-    // Your implementation here...
-
 }
